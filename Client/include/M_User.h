7716ed51820d57291c5bd655cfa61aeadaaa63c1
kakadiyahitesh20 <kakadiyahitesh20@gmail.com>
2017-05-04 16:00:50 +0530

    Added Adding and removing user functions.

---
+++ Client/include/M_User.h
@@ -0,0 +1,44 @@
+#ifndef SERVER_M_USER_H
+#define SERVER_M_USER_H
+#include<vector>
+#include <iostream>
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
+#include <fstream>
+#include <sstream>
+using namespace std;
+static char *Cptr;
+class M_User
+{
+public:
+    void adduser();
+    void deleteUser();
+    void ipaddress(char*);
+    string const send2Server(string);
+    void DisplayUserdump(M_User&,int);
+    friend class boost::serialization::access;
+
+    template<typename Archive>
+
+    void serialize(Archive &ar, const unsigned int version) {
+
+        ar & name;
+        ar & email;
+        ar & phone;
+        ar & uname;
+    }
+
+private:
+    string name,uname,email,phone;
+
+protected:
+    vector<M_User> userData;
+};
+
+
+#endif
---
+++ Client/src/M_User.cpp
@@ -0,0 +1,72 @@
+#include "../include/M_User.h"
+typedef boost::asio::ip::tcp asiotcp;
+
+void M_User::ipaddress(char *Copy) {
+        Cptr=Copy;
+}
+void M_User::DisplayUserdump(M_User& output,int i) {
+
+        ostream &os = cout;
+        os << "User:"<<++i<<endl;
+        os << "******************************************" << std::endl;
+        os << "\tUsername: " << output.uname<< endl;
+        os << "\tName :    " << output.name << endl;
+        os << "\tEmail:    " << output.email<<endl;
+        os << "\tPhone:    " << output.phone<<endl;
+        os << "******************************************" << std::endl;
+}
+
+string const M_User::send2Server(string output) {
+
+        boost::asio::io_service io_service;
+        asiotcp::endpoint remote_endpoint = asiotcp::endpoint(boost::asio::ip::address_v4::from_string(Cptr),13);
+        asiotcp::socket socket(io_service);
+        socket.open(asiotcp::v4());
+        socket.connect(remote_endpoint);
+        socket.send(boost::asio::buffer(output));
+
+        array<char, 2048> recv_buf;
+        size_t const received_bytes = socket.receive(boost::asio::buffer(recv_buf));
+        string const inputmessage(recv_buf.data(), received_bytes);
+        return inputmessage;
+}
+
+void M_User::adduser() {
+        try {
+                M_User obj;
+                stringstream ss;
+                cout << "------------Add User------------" << std::endl;
+                cin.ignore();
+                cout << "Enter First Name:" << std::endl;
+                getline(cin, obj.name);
+                cout << "Enter User Email:" << std::endl;
+                getline(cin, obj.email);
+                cout << "Enter User name:" << std::endl;
+                getline(cin, obj.uname);
+                cout << "Enter Phone Number:" << endl;
+                getline(cin, obj.phone);
+                boost::archive::text_oarchive oa{ss};
+                oa << obj;
+                string output = "1|" + ss.str();
+                string res=obj.send2Server(output);
+                cout<<"Server->"<<"Username:"<<res<<endl;
+        }
+        catch (exception const &e)
+        {
+                cerr << e.what() <<endl;
+        }
+}
+
+void M_User::deleteUser() {
+    stringstream ss;
+    M_User delete_User;
+    cout << "------------Delete User------------" << std::endl;
+    cin.ignore();
+    cout<<"Please enter username to delete:"<<endl;
+    getline(cin,delete_User.uname);
+    boost::archive::text_oarchive oa{ss};
+    oa << delete_User;
+    string output = "4|" + ss.str();
+    string res=delete_User.send2Server(output);
+    cout<<"Server->"<<res<<endl;
+}
